7e23c9aeae34340eb359bb3900b8a8a7e1b8e986
Dale Newman <dalenewman@gmail.com>
2019-05-07 12:50:23 -0400

    update nuget version

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,8 +1,8 @@
 nuget pack Transformalize.Provider.Elasticsearch.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.Elasticsearch.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.0.3.25-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.0.3.25-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.0.6.4-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.0.4.4-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
 
