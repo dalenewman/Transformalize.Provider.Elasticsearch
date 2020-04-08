5df94bb43cde7fee7b58d1370c877f6f07ef930e
Dale Newman <dalenewman@gmail.com>
2020-04-08 10:34:29 -0400

    added .net standard 2.0 target for provider, still need to add it for autofac module

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,11 +1,11 @@
 nuget pack Transformalize.Provider.Elasticsearch.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Provider.Elasticsearch.Autofac.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Provider.Elasticsearch.Autofac.v3.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Provider.Elasticsearch.Autofac.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Provider.Elasticsearch.Autofac.v3.nuspec -OutputDirectory "c:\temp\modules"
 
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.0.6.20-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.0.6.20-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.v3.0.6.20-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.0.6.28-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.0.6.28-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.v3.0.6.28-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
 
