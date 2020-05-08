3e6e8da263b17d26af569dc780ec3ee665dc46a8
Dale Newman <dalenewman@gmail.com>
2020-05-08 13:39:10 -0400

    push myget packages

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -3,9 +3,9 @@ nuget pack Transformalize.Provider.Elasticsearch.Autofac.nuspec -OutputDirectory
 nuget pack Transformalize.Provider.Elasticsearch.Autofac.v3.nuspec -OutputDirectory "c:\temp\modules"
 
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.0.6.28-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.0.6.28-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.v3.0.6.28-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.0.7.6-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.0.7.6-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.v3.0.7.6-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
 
