72e4543475114e35d326f2c833efd08ea563ca72
Dale Newman <dalenewman@gmail.com>
2020-10-01 14:32:31 -0400

    Update Dependencies

--- nuget/myget.bat
+++ nuget/myget.bat
@@ -2,9 +2,9 @@ REM nuget pack Transformalize.Provider.Elasticsearch.nuspec -OutputDirectory "c:
 REM nuget pack Transformalize.Provider.Elasticsearch.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 REM nuget pack Transformalize.Provider.Elasticsearch.Autofac.v3.nuspec -OutputDirectory "c:\temp\modules"
 
-nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.v3.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.0.8.26-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.0.8.26-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.v3.0.8.26-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
 
 
--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -2,9 +2,9 @@ nuget pack Transformalize.Provider.Elasticsearch.nuspec -OutputDirectory "c:\tem
 nuget pack Transformalize.Provider.Elasticsearch.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.Elasticsearch.Autofac.v3.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.0.8.22-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.0.8.22-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.v3.0.8.22-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.0.8.26-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.0.8.26-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Elasticsearch.Autofac.v3.0.8.26-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
 
